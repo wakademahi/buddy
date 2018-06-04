df531af966427d31ef2e743fef0c41ae9c30c9f8
Mahesh Wakade <33047369+wakademahi@users.noreply.github.com>
2018-06-04 14:37:24 +0530

    Update environment.ts

--- src/providers/auth/environment.ts
+++ src/providers/auth/environment.ts
@@ -1,6 +1,6 @@
 import 'rxjs/add/operator/map';
 
 export const environment = {
-    //origin: 'http://97.74.234.178:8090/'
-    origin: 'http://' + localStorage.getItem('domain') + '/'
+    origin: 'http://97.74.234.178:7071/'
+   //origin: 'http://' + localStorage.getItem('domain') + '/'
 };
